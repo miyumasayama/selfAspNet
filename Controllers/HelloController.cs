a1c7f8a937b6b1952dee36fee1f88b8dcb7d2b20
miyumasayama <921ohagi@gmail.com>
2025-05-25 20:20:03 +0900

    fix:HelloControllerにlistメソッドを追加

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -1,9 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using SelfAspNet.Models;
 
 namespace SelfAspNet.Controllers
 {
   public class HelloController : Controller
   {
+    private readonly MyContext _db;
+    public HelloController(MyContext db)
+    {
+      _db = db;
+    }
+
+    public IActionResult List()
+    {
+      var books = _db.Books;
+      return View(books);
+    }
     public IActionResult Index()
     {
       return View();
